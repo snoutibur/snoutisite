b84c625f6eda54069f4df8f2cf6e6146b8032c93
snoutibur <157987813+snoutibur@users.noreply.github.com>
2025-06-23 17:15:53 +0800

    Removed astro/images

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,4 @@
 import {defineConfig} from 'astro/config';
-import image from '@astrojs/image';
 
 // https://astro.build/config
 export default defineConfig({
@@ -12,6 +11,4 @@ export default defineConfig({
             }
         }
     },
-
-    integrations: [image()],
 });
